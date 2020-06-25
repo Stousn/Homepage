36408efdb41bb5f77decf9cba114f2b41cdcc82e
Stefan Reip <stefan.reip@r-software.at>
2020-06-25 13:06:02 +0200

    master: added freiberuflichkeit

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,49 @@
 function w3_open() {
-  document.getElementById("mySidebar").style.width = "45%";
-  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById('mySidebar').style.width = '45%'
+  document.getElementById('mySidebar').style.display = 'block'
 }
 function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById('mySidebar').style.display = 'none'
+}
+function setNames() {
+  var names = document.getElementsByTagName('name')
+  for (var i = 0; i < names.length; i++) {
+    names[i].innerText = document.title
+  }
 }
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
+  z = document.getElementsByTagName('*')
   for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
+    elmnt = z[i]
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+    file = elmnt.getAttribute('w3-include-html')
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
+      xhttp = new XMLHttpRequest()
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) {elmnt.innerHTML = this.responseText}
+          if (this.status == 404) {elmnt.innerHTML = 'Page not found.'}
           /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
+          elmnt.removeAttribute('w3-include-html')
+          includeHTML()
+          setNames()
         }
       }
-      xhttp.open("GET", file, true);
-      xhttp.send();
+      xhttp.open('GET', file, true)
+      xhttp.send()
       /*exit the function:*/
       return;
     }
   }
 }
-includeHTML();
+includeHTML()
 
 function loadContent(page, name) {
-  document.getElementById("content").setAttribute("w3-include-html", "content/" + page);
-  includeHTML();
-  document.getElementById("pagename").innerHTML = name;
-  w3_close();
+  document.getElementById('content').setAttribute('w3-include-html', 'content/' + page)
+  includeHTML()
+  document.getElementById('pagename').innerHTML = name
+  w3_close()
 }
