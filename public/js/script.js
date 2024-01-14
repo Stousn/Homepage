36abf83d20e4da66467f4a877b381968bbcae7cc
Stousn <stefan.reip@edu.fh-joanneum.at>
2024-01-14 19:36:04 +0100

    add overlay to close navbar per outside click

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,11 @@
 function w3_open() {
   document.getElementById('mySidebar').style.width = '45%'
   document.getElementById('mySidebar').style.display = 'block'
+  document.getElementById('overlay').style.display = 'block'
 }
 function w3_close() {
   document.getElementById('mySidebar').style.display = 'none'
+  document.getElementById('overlay').style.display = 'none'
 }
 function setNames() {
   let names = document.getElementsByTagName('name')
